01be6d8a58c0d8b5e2bec8ac4fa5fa0012b17e6b
Jeffrey Sta-Ines <jeffsantaines@gmail.com>
2017-08-01 22:42:55 +1200

    Changed ''Ten" to "10"

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,27 +18,13 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         //Create an array of English words from one to ten
-        ArrayList<String> englishWords = new ArrayList<String>(Arrays.asList(
+        ArrayList<String> englishWords = new ArrayList<>(Arrays.asList(
                 "one", "two", "three", "four", "five", "six",
-                "seven", "eight", "nine", "ten"));
+                "seven", "eight", "nine", "10"));
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, englishWords);
-
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, englishWords);
         ListView listView = (ListView) findViewById(R.id.list);
-
         listView.setAdapter(itemsAdapter);
 
-
-
-        /*for ( int index = 0; index < englishWords.size(); index++) {
-            TextView newTextView = new TextView(this);
-            newTextView.setText(englishWords.get(index));
-           *//* newTextView.setHeight(50);
-            newTextView.setWidth(200);
-            newTextView.setBackgroundColor(123);*//*
-            rootView.addView(newTextView);
-            textViewArrayList.add(newTextView);
-
-        }*/
     }
 }
